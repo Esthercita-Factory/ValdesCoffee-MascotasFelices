0d7c11b7576e5c517fa714a341a3ec7e9e0d1671
ValdesCoffee <valdes.2022806@ensdbexcelencia.edu.co>
2026-08-28 14:10:43 -0500

    agregando cambio a IAtendible

--- Services/ServicioVeterinario.cs
+++ Services/ServicioVeterinario.cs
@@ -1,10 +1,8 @@
-using ClinicaPatitasFelices.Console.Interfaces;
 using ClinicaPatitasFelices.Console.Models;
 
 namespace ClinicaPatitasFelices.Console.Services
 {
-    // Implementa IAtendible: toda subclase queda obligada a definir su propio Atender().
-    public abstract class ServicioVeterinario : IAtendible
+    public abstract class ServicioVeterinario
     {
         public DateTime Fecha { get; set; } = DateTime.Now;
 
--- obj/Debug/net10.0/ClinicaPatitasFelices.Console.AssemblyInfo.cs
+++ obj/Debug/net10.0/ClinicaPatitasFelices.Console.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicaPatitasFelices.Console")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36ca7ae22d87775cf0b35c6f705b95d01e8f7f8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+222cb84951d80d60a9431a709a62bb6546a9d717")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicaPatitasFelices.Console")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicaPatitasFelices.Console")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
